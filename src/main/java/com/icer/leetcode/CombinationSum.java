13d602f80a0d1ece60e5486dd573bf13fd6e463a
zhangbing <zhangbing@do-global.com>
2019-07-17 18:00:13 +0800

    feature:39题 combinationSum

--- src/main/java/com/icer/leetcode/CombinationSum.java
+++ src/main/java/com/icer/leetcode/CombinationSum.java
@@ -1,5 +1,43 @@
 package com.icer.leetcode;
 
-public class CombinationSum {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+/**
+ * 39. Combination Sum
+ */
+public class CombinationSum {
+    public static void main(String[] args) {
+        CombinationSum app = new CombinationSum();
+        List<List<Integer>> lists = app.combinationSum(new int[]{2, 3, 4, 5}, 7);
+        System.out.println(lists);
+    }
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> results = new ArrayList<>();
+        if (candidates == null || candidates.length == 0) {
+            return results;
+        }
+        Arrays.sort(candidates);
+        List<Integer> combinations = new ArrayList<>();
+        findCombinationToTarget(results, combinations, candidates, target, 0);
+        return results;
+    }
+
+    private void findCombinationToTarget(List<List<Integer>> results, List<Integer> combinations, int[] candidates, int target, int startIndex) {
+        if (target == 0) {
+            results.add(new ArrayList<>(combinations));
+            return;
+        }
+
+        for (int i = startIndex; i < candidates.length; i++) {
+            if (candidates[i] > target) {
+                break;
+            }
+            combinations.add(candidates[i]);
+            findCombinationToTarget(results, combinations, candidates, target - candidates[i], i);
+            combinations.remove(combinations.size() - 1);
+        }
+    }
 }
