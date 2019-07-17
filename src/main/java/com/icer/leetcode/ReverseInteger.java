aedda0789ec08b745954c8c00c7aadd8058cf1a0
zhangbing <zhangbing@do-global.com>
2019-07-17 20:58:04 +0800

    第9题 reverseInteger

---
+++ src/main/java/com/icer/leetcode/ReverseInteger.java
@@ -0,0 +1,17 @@
+package com.icer.leetcode;
+
+public class ReverseInteger {
+    public int reverse(int x) {
+        int reverse = 0;
+        while (x != 0) {
+            int temp = reverse * 10 + x % 10;
+            if (temp / 10 != reverse) {
+                //防止overflow
+                return 0;
+            }
+            reverse = temp;
+            x = x / 10;
+        }
+        return reverse;
+    }
+}
