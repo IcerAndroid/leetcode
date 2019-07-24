3c64923a77502bee78679ba704518809c6b4c426
zhangbing <zhangbing@do-global.com>
2019-07-24 17:13:51 +0800

    56. Merge Intervals 合并数组区间

--- src/main/java/com/icer/leetcode/MaximumSubarray.java
+++ src/main/java/com/icer/leetcode/MaximumSubarray.java
@@ -18,11 +18,11 @@ public class MaximumSubarray {
         }
         return max;
     }
-
-    public static void main(String[] args) {
-        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        MaximumSubarray app = new MaximumSubarray();
-        int max = app.maxSubArray(arr);
-        System.out.println(max);
-    }
+//
+//    public static void main(String[] args) {
+//        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+//        MaximumSubarray app = new MaximumSubarray();
+//        int max = app.maxSubArray(arr);
+//        System.out.println(max);
+//    }
 }
